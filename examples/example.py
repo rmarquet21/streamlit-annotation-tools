616cf448961ea2129721b8575e096f7f0f746a07
Robin Marquet <robin.marquet@epitech.eu>
2023-05-13 01:28:11 +0200

    chore: update example to create app streamlit

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,19 @@
+import streamlit as st
 from st_text_annotator import StTextAnnotator
 
-text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et hendrerit orci. Praesent auctor malesuada lobortis. Suspendisse ac elit bibendum, congue tellus vel, ornare ipsum. Mauris at tellus in turpis aliquet cursus."
+st.markdown("# Text Annotation Tool")
+st.markdown("This is a Streamlit component that provides a text annotation tool. With this tool, users can highlight text, add annotations to it, and group annotations under different references.")
 
-StTextAnnotator(text, [])
+# add link to repo and pyPi
+st.markdown("## Links")
+st.markdown("Github: [https://github.com/rmarquet21/st-text-annotator](https://github.com/rmarquet21/st-text-annotator)")
+st.markdown("PyPi: [https://pypi.org/project/st-text-annotator/](https://pypi.org/project/st-text-annotator/)")
+
+# add example
+st.markdown("## Example")
+
+text = st.text_input("Enter the text you want to annotate")
+
+if text:
+    StTextAnnotator(text, [])
 
